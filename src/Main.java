65a061d22f57c4e572b3cc8bfc69823e06490038
malkiro <malkiroshika3@gmail.com>
2023-02-11 18:41:57 +0530

    Space Added2

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         int x = 90;
-        String name = "Rooo";
+        String name = " Roo ";
     }
 }
